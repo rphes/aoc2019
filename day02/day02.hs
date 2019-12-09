4f4d500a0ab016367356e694858507cdfc1faa2a
Robin Hes <robinhes@outlook.com>
2019-12-09 21:52:48 +0100

    Refactored intcode computer to use IntMaps.

--- day02/day02.hs
+++ day02/day02.hs
@@ -1,45 +1,44 @@
 import System.IO
+import Data.IntMap.Strict (IntMap, (!))
+import qualified Data.IntMap.Strict as IntMap
 import Data.List.Split
-import Debug.Trace
 
-update :: Int -> a -> [a] -> [a]
-update at val xs = let (begin, _:end) = splitAt at xs
-                    in begin ++ (val : end)
-
-exec :: Int -> Int -> Int -> Int
-exec 1 a b = a + b
-exec 2 a b = a * b
-
-step :: [Int] -> Int -> [Int]
+step :: IntMap Int -> Int -> IntMap Int
 step prog counter | counter < length prog =
-  let (op:rest) = drop counter prog
-   in case op of
-        99 -> prog
-        otherwise ->
-          let (a':b':dest:_) = rest
-              a = prog!!a'
-              b = prog!!b'
-           in step (update dest (exec op a b) prog) (counter + 4)
+  let inst = prog!counter
+      a = prog!(prog!(counter + 1))
+      b = prog!(prog!(counter + 2))
+      dest = prog!(counter + 3)
+      exec op = step (IntMap.insert dest (a `op` b) prog) (counter + 4) in
+        case inst of
+          1 -> exec (+)
+          2 -> exec (*)
+          99 -> prog
+          _ -> error "Unsupported instruction"
 step prog counter | otherwise = prog
 
-run :: [Int] -> Int -> Int -> Int
-run prog noun verb = head $ step ( update 1 noun $ update 2 verb prog ) 0
+run :: IntMap Int -> Int -> Int -> Int
+run prog noun verb = (step (
+  IntMap.insert 1 noun $
+    IntMap.insert 2 verb
+  prog) 0) ! 0
 
 part1 prog = run prog 12 2
 
-gridsearch :: [Int] -> [(Int, Int)] -> (Int, Int)
-gridsearch prog ((noun, verb):space) = let res = run prog noun verb
-                                        in case res of
-                                             19690720 -> (noun, verb)
-                                             otherwise -> gridsearch prog space
+gridsearch :: IntMap Int -> Int -> [(Int, Int)] -> (Int, Int)
+gridsearch prog value ((noun, verb):space)
+  | res == value = (noun, verb)
+  | res /= value  = gridsearch prog value space
+  where res = run prog noun verb
 
 part2 prog = let range = [0..99]
                  space = [(x, y) | x <- range, y <- range]
-                 (noun, verb) = gridsearch prog space
+                 (noun, verb) = gridsearch prog 19690720 space
               in 100 * noun + verb
 
 main = do
   contents <- readFile "input.txt"
-  let prog = map read (splitOn "," contents)::[Int]
+  let prog = IntMap.fromList $ zip [0..] $ (map read (splitOn "," contents) :: [Int])
+
   print $ "Part 1: " ++ (show $ part1 prog)
   print $ "Part 2: " ++ (show $ part2 prog)
