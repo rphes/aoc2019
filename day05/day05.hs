4cab4a4098a541525b7c8f1c33842e5522e678ea
Robin Hes <robinhes@outlook.com>
2019-12-17 20:59:01 +0100

    Day 5 part 2.

--- day05/day05.hs
+++ day05/day05.hs
@@ -30,15 +30,76 @@ eval prog counter input output = do
   let getReg = flip IntMap.lookup prog
   (opcode, modes) <- fmap decode (getReg counter)
   let getMode = flip mode modes
+  let dest = getReg (counter + 3)
   case opcode of
-    3 -> do
+    3
+      -- load input
+     -> do
       let (x:xs) = input
       dest <- getReg (counter + 1)
       eval (IntMap.insert dest x prog) (counter + 2) xs output
-    4 -> do
+    4
+      -- store output
+     -> do
       p <- getReg (counter + 1)
       val <- load prog (getMode 0) p
       eval prog (counter + 2) input (val : output)
+    5
+      -- jump-if-true
+     -> do
+      a <- (getReg >=> load prog (getMode 0)) (counter + 1)
+      b <- (getReg >=> load prog (getMode 1)) (counter + 2)
+      eval
+        prog
+        (if a /= 0
+           then b
+           else counter + 3)
+        input
+        output
+    6
+      -- jump-if-false
+     -> do
+      a <- (getReg >=> load prog (getMode 0)) (counter + 1)
+      b <- (getReg >=> load prog (getMode 1)) (counter + 2)
+      eval
+        prog
+        (if a == 0
+           then b
+           else counter + 3)
+        input
+        output
+    7
+      -- less than
+     -> do
+      a <- (getReg >=> load prog (getMode 0)) (counter + 1)
+      b <- (getReg >=> load prog (getMode 1)) (counter + 2)
+      dest <- getReg (counter + 3)
+      eval
+        (IntMap.insert
+           dest
+           (if a < b
+              then 1
+              else 0)
+           prog)
+        (counter + 4)
+        input
+        output
+    8
+      -- equals
+     -> do
+      a <- (getReg >=> load prog (getMode 0)) (counter + 1)
+      b <- (getReg >=> load prog (getMode 1)) (counter + 2)
+      dest <- getReg (counter + 3)
+      eval
+        (IntMap.insert
+           dest
+           (if a == b
+              then 1
+              else 0)
+           prog)
+        (counter + 4)
+        input
+        output
     99 -> Just (reverse output, prog)
     _ -> do
       a <- (getReg >=> load prog (getMode 0)) (counter + 1)
@@ -56,14 +117,26 @@ initProg = IntMap.fromList . zip [0 ..]
 
 run prog input = eval (initProg prog) 0 (reverse input) []
 
+verifyProg prog input expected =
+  assert (snd (fromJust $ run prog input) == initProg expected) return ()
+
+verifyOutput prog input expected =
+  assert (fst (fromJust $ run prog input) == expected) return ()
+
 main = do
   let test1 = [3, 0, 4, 0, 99]
       test2 = [1002, 4, 3, 4, 33]
-  assert (fst (fromJust (run test1 [1])) == [1]) return ()
-  assert
-    (snd (fromJust (run test2 [1])) == initProg [1002, 4, 3, 4, 99])
-    return
-    ()
+      test3 = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+      test4 = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+      test5 = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+      test6 = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+  verifyOutput test1 [1] [1]
+  verifyProg test2 [1] [1002, 4, 3, 4, 99]
+  verifyOutput test3 [8] [1]
+  verifyOutput test4 [8] [0]
+  verifyOutput test5 [7] [0]
+  verifyOutput test6 [1] [1]
   contents <- readFile "input.txt"
   let prog = map read (splitOn "," contents) :: [Int]
   print $ fmap fst (run prog [1])
+  print $ fmap fst (run prog [5])
